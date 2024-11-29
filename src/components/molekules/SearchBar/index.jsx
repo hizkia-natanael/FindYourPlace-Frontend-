d7ca3dd9d69142c5cfd64f37a81672d2efc53e14
fathurrahman-24 <faturrahman2004678@gmail.com>
2024-11-29 10:46:25 +0700

    Update hero dan search bar

--- src/components/molekules/SearchBar/index.jsx
+++ src/components/molekules/SearchBar/index.jsx
@@ -1,9 +1,14 @@
 import { Input, Button } from "../../atoms";
 
-const SearchBar = () => {
+const SearchBar = ({ value, onChange }) => {
   return (
     <div className="flex">
-      <Input className="flex-grow w-full" placeholder="Cari Tempat Nongkrong" />
+      <Input
+        className="flex-grow w-full"
+        placeholder="Cari Tempat Nongkrong"
+        value={value} // Bind the value prop
+        onChange={onChange} // Bind the onChange handler
+      />
       <Button className="ml-2 bg-[#c66e4e] text-white">Cari</Button>
     </div>
   );
